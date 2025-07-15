88db65165cb8ca7be5515513f394162c9802920d
Luiz Henrique <seniorlulu20@gmail.com>
2025-07-15 17:18:09 -0300

    Remoção dos comentarios desnecessarios

--- backend/routes/livroRoutes.js
+++ backend/routes/livroRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const livroController = require('../controllers/livroController');
 
-// POST /api/livros
 router.post('/livros', livroController.criarLivro);
 
-// GET /api/livros
 router.get('/livros', livroController.listarLivros);
 
 module.exports = router;
